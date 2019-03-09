c8913430b640e8ed10eab97b8d28d6fe584e0d93
Patrick Levy <mrpatricklevy@gmail.com>
2019-03-09 09:57:38 -0600

    Try connecting to remote server

--- appscript.js
+++ appscript.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = 'http://localhost:3001'
+//const API_BASE_URL = 'http://localhost:3001'
+const API_BASE_URL = 'http://138.68.40.165:3001'
 
 function sayHello () {
   console.log('hello from a function on the front end')
