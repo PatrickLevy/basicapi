70763f0e7420c7cb5d951957d1f20201681ca9dc
Patrick Levy <mrpatricklevy@gmail.com>
2019-03-09 09:32:41 -0600

    Add html files

---
+++ appscript.js
@@ -0,0 +1,37 @@
+const API_BASE_URL = 'http://localhost:3001'
+
+function sayHello () {
+  console.log('hello from a function on the front end')
+}
+
+/*******************************
+ * When Document is Loaded...
+ *******************************/
+document.addEventListener("DOMContentLoaded", function(event) { 
+  console.log('document ready, starting script...');
+  
+  // Start running code (open browser dev tools console to see this)
+  sayHello();
+
+  // Make a request to the backend server
+  // see: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  const requestEndpoint = API_BASE_URL + '/arbitrary'; 
+  fetch(requestEndpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      // Do some stuff with your data, put it on your page, etc.
+      // For now, check your browser console and you should see it
+      console.log(myJson);
+      $('#dataWillGoHere').text(myJson.data.message)
+    })
+    .catch(function(error) {
+      console.error(error);
+      $('#dataWillGoHere').text('We are not connected to our API Server!')
+    });
+
+});
+
+
+
